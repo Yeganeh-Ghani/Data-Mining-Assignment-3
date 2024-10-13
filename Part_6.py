375ada305feecc34ab38652594500c6709b5acdf
Yeganeh-Ghani <yeganehghani402@gmail.com>
2024-10-13 17:49:21 +0330

    Calculate Correlation Coefficient and its Type

--- Part_6.py
+++ Part_6.py
@@ -7,6 +7,37 @@ Fat_data =[9.5, 26.5, 7.8, 17.8, 31.4, 25.9, 27.4, 27.2, 31.2, 34.6, 42.5, 28.8,
 
 ################################################## FUNCTIONS ##################################################
 
+def Arthmetic_Mean(d): # d-> data
+    return sum(d) / len(d)
 
+def Variance(data):
+    mean = Arthmetic_Mean(data)
+    squered_diff_sum = sum((x - mean) ** 2 for x in data) # The sum of the squared deviations
+
+    return squered_diff_sum / (len(data) - 1)
+
+def Standard_Deviation(data):
+    return Variance(data) ** 0.5 # The root of the variance
+
+def Covariance(data1, data2):
+    mean1 = Arthmetic_Mean(data1)
+    mean2 = Arthmetic_Mean(data2)
+    cov_sum = sum((data1[i] - mean1) * (data2[i] - mean2) for i in range(len(data1)))
+    
+    return cov_sum / (len(data1) - 1)
+
+def Correlation_Coefficient(data1, data2):
+    cov = Covariance(data1, data2)
+    std_dev1 = Standard_Deviation(data1)
+    std_dev2 = Standard_Deviation(data2)
+
+# Type of Correlation Coefficient
+def Correlation_Type(corr):
+    if corr > 0:
+        return "Positive Correlation."
+    elif corr < 0:
+        return "Negative Correlation."
+    else:
+        return "No Correlation."
 
 ################################################## OUTPUT ##################################################
