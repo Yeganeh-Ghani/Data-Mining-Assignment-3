272908044f0ae28b7aa6fe607c72c3fdd9acd3ca
Yeganeh-Ghani <yeganehghani402@gmail.com>
2024-10-15 03:36:32 +0330

    Binning Methods
    Finished Section 5

--- Part_5.py
+++ Part_5.py
@@ -1,5 +1,8 @@
 import math
 import numpy as np
+from colorama import Fore, Style, init
+
+init()
 
 ################################################## DATA ##################################################
 
@@ -118,3 +121,82 @@ def Identify_Outliers_lib(data):
     return outliers, lower_bound, upper_bound, mean, st_Dev
 
 ################################################## OUTPUT ##################################################
+
+mean = Arthmetic_Mean(Age_data)
+median = Median(Age_data)
+mode = Mode(Age_data)
+n = len(Age_data)
+R = max(Age_data) - min(Age_data)
+K = round(1 + 3.3 * math.log(n)) # Count of groups
+D = round(R / K) # Distance between Groups
+
+print(Fore.BLUE, "Mean : ", Fore.WHITE, mean)
+print(Fore.BLUE, "Median : ", Fore.WHITE, median)
+print(Fore.BLUE, "Mode : ", Fore.WHITE, mode)
+
+print(Fore.LIGHTBLACK_EX, "----------------------------------------------------------------------------------------------------")
+
+q1, q2, q3, iqr = Quartile(Age_data)
+
+print(Fore.BLUE, "Quartile 1 : ", Fore.WHITE, q1)
+print(Fore.BLUE, "Quartile 2 : ", Fore.WHITE, q2)
+print(Fore.BLUE, "Quartile 3 : ", Fore.WHITE, q3)
+print(Fore.BLUE, "Quartile Deviation : ", Fore.WHITE, iqr)
+
+print(Fore.LIGHTBLACK_EX, "----------------------------------------------------------------------------------------------------")
+
+outliers, lower, upper, mean1, std = Identify_Outliers_lib(Age_data)
+
+print(Fore.BLUE, "Outlier : ", Fore.WHITE, outliers)
+print(Fore.BLUE, "Lower Bound : ", Fore.WHITE, lower)
+print(Fore.BLUE, "Upper Bound : ", Fore.WHITE, upper)
+print(Fore.BLUE, "Mean : ", Fore.WHITE, mean1)
+print(Fore.BLUE, "Standard Deviation : ", Fore.WHITE, std)
+
+print(Fore.LIGHTBLACK_EX, "----------------------------------------------------------------------------------------------------")
+
+print(Fore.BLUE, "Number of Groups : ", Fore.WHITE, K) # 12
+print(Fore.BLUE, "Distance between Groups : ", Fore.WHITE, D) # 6
+
+list_1 = (3) # mean = 3
+list_2 = (13, 15, 16, 16) # mean = 15
+list_3 = (19, 20, 20, 21, 22, 22) # mean = 20.6 = 21
+list_4 = (25, 25, 25) # mean = 25
+list_5 = (30) # mean = 30
+list_6 = (33, 33) # mean = 33
+list_7 = (35, 35, 35, 35) # mean = 35
+list_8 = (36) # mean = 36
+list_9 = (40) # mean = 40
+list_10 = (45, 46) # mean = 68
+list_11 = (52) # mean = 52
+list_12 = (70) # mean = 70
+
+final_list = [list_1, list_2, list_3, list_4, list_5, list_6, list_7, list_8, list_9, list_10, list_11, list_12]
+
+print(Fore.BLUE, "First Binning by Calculate K in 12 Groups (Slide 27, 28) : ", Fore.WHITE)
+for i in final_list:
+    print(i)
+
+error = 0 + math.fabs(13-15) + 1 + 1 + math.fabs(19-21) + math.fabs(20-21) + math.fabs(20-21) + 1 + 1 + math.fabs(45-68) + math.fabs(46-68)
+
+print(Fore.BLUE, "Error in Group 1 : ", Fore.WHITE, error) # 55
+
+t_1 = (3) # mode = 3, mean = 3
+t_2 = (13, 15, 16, 16, 19) # mode = 16, mean = 15.8 = 16
+t_3 = (20, 20, 21, 22, 22, 25, 25, 25) # mode = 25, mean = 22.5 = 25
+t_4 = (30, 33, 33, 35, 35, 35, 35, 36) # mode = 35, mean = 29.5 = 30
+t_5 = (40, 45, 46) # mode = 4, 45, 46, mean = 43.6 = 44
+t_6 = (52) # mdoe = 52, mean = 52
+t_7 = (70) # mode = 70, mean = 70
+
+final_list1 = [t_1, t_2, t_3, t_4, t_5, t_6, t_7]
+
+print(Fore.BLUE, "\nSecond Binning by Mean in 12 Groups (Slide 29) : ", Fore.WHITE)
+for i in final_list1:
+    print(i)
+
+error1 = math.fabs(13-16) + math.fabs(15-16) + (19-16) + math.fabs(20-25) + math.fabs(20-25)
++ math.fabs(21-25) + math.fabs(22-25) + math.fabs(22-25) + (33-30) + (33-30) + (35-30) + (35-30) + (35-30) + (35-30)
++ math.fabs(40-44) + math.fabs(45-44) + math.fabs(46-44) 
+
+print(Fore.BLUE, "Error in Group 2 : ", Fore.WHITE, error1) # 17
